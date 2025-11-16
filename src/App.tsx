210cce7d5827b6f04262b30023871cb6a09d3f6f
tasnim-rahman <tasnimrahman0101@gmail.com>
2025-11-16 17:29:12 +0600

    install tailwind, setup redux toolkit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <div className="m-10">
+        <h1 className="text-xl">My ToDo</h1>
+        <Button>Click Me!</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
